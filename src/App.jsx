c93868a5476524ff53b25a23c760787ac80ce12d
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2025-03-09 00:03:31 +0530

    update person to udis var

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    // personal data state to store data from personal form section
+    const [personalData, setPersonalData] = useState({
+        firstName: "First name",
+        lastName: "Last Name",
+        email: "Email",
+        number: "Phone Number",
+        position: "Current Position"
+    })
+
+    // Stores data from personal info form into state var
+    function handlePersonalInfo(formData) {
+        setPersonalData({
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
+            email: formData.get('email'),
+            position: formData.get('position'),
+            number: formData.get('number')
+        })
+    }
 
     return (<>
         {/*1 row 2 cols*/}
@@ -13,12 +29,14 @@ function App() {
             <div className="form-container container-style">
                 <div>
                     <h3>Personal Information</h3>
-                    <form className="personal-info form-style">
-                        <input type={"text"} placeholder="First Name"/>
-                        <input type={"text"} placeholder="Last Name"/>
-                        <input type={"email"} placeholder="email"/>
-                        <input type={"number"} placeholder="Phone Number"/>
-                        <input type={"text"} placeholder="Curent Position"/>
+
+                    <form className="personal-info form-style" action={handlePersonalInfo}>
+                        <input required={true} type={"text"} name={'firstName'} placeholder={personalData.firstName}/>
+                        <input required={true} type={"text"} name={'lastName'} placeholder={personalData.lastName}/>
+                        <input required={true} type={"email"} name={'email'} placeholder={personalData.email}/>
+                        <input required={true} type={"number"} name={'number'} placeholder={personalData.number}/>
+                        <input required={true} type={"text"} name={'position'} placeholder={personalData.position}/>
+                        <button type={"submit"} onSubmit={e => e.preventDefault()}>Submit</button>
                     </form>
                 </div>
                 <div>
@@ -31,6 +49,7 @@ function App() {
                         <input type={"email"} placeholder="Institute Email"/>
                         <input type={"text"} placeholder="Institute Address"/>
                         <input type={"text"} placeholder="Course Completed"/>
+                        <button type={"submit"}>Submit</button>
                     </form>
                 </div>
                 <div>
@@ -43,6 +62,7 @@ function App() {
                         <input type={"email"} placeholder="Company Email"/>
                         <input type={"text"} placeholder="Company Address"/>
                         <input type={"text"} placeholder="Position"/>
+                        <button type={"submit"}>Submit</button>
                     </form>
                 </div>
 
@@ -50,9 +70,19 @@ function App() {
             </div>
             {/*3 row 1 cols*/}
             <div className="view-container container-style">
-                {/*<div className="form-view personal-view"></div>*/}
-                {/*<div className="form-view education-view"></div>*/}
-                {/*<div className="form-view exp-view"></div>*/}
+                <div className="form-view personal-view">
+                    <h3>{personalData.firstName}</h3>
+                    <h3>{personalData.lastName}</h3>
+                    <h3>{personalData.email}, {personalData.position}, {personalData.number}</h3>
+                </div>
+                <div className="form-view education-view">
+                    <h3>Institute name</h3>
+                    <h3> Institute email, Institute address, course</h3>
+                </div>
+                <div className="form-view exp-view">
+                    <h3>Co name</h3>
+                    <h3> CO email, CO address, Position</h3>
+                </div>
             </div>
         </div>
     </>)
